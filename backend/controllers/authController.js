af262f53060821f12d7c350779bbcaa8f612d7f9
mbmb <phamminhduc2011@gmail.com>
2025-12-05 09:04:25 +0700

    add authentication API

---
+++ backend/controllers/authController.js
@@ -0,0 +1,66 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+//Generate Token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.SECRET_KEY, {expiresIn: '60d'});
+};
+
+// Register User
+exports.register = async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
+        const userExists = await User.findOne({email});
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const user = await User.create({ name, email, password, role });
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateToken(user._id),
+            companyName: user.companyName,
+            companyDescription: user.companyDescription,
+            companyLogo: user.companyLogo,
+            resume: user.resume || ' ',
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+// Login User
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (user && (await user.matchPassword(password))) {
+            return res.status(401).json({message: 'Invalid email or password' });
+        }
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateToken(user._id),
+            avatar: user.avatar || ' ',
+            companyName: user.companyName,
+            companyDescription: user.companyDescription,
+            companyLogo: user.companyLogo,
+            resume: user.resume || ' ',
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+// Get current logged in user
+exports.getMe = async (req, res) => {
+    res.json(req.user);
+};
